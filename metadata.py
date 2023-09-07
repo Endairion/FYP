f74e4a2ce2069aca2a5fc299bd4cd4e5422f28df
Endairion <86313950+Endairion@users.noreply.github.com>
2023-09-07 11:31:20 +0800

    hot fix

--- metadata.py
+++ metadata.py
@@ -1,10 +1,9 @@
 # metadata.py
 
 class FileMetadata:
-    def __init__(self, file_id, fragment_id, fragment_index, fragment_size, fragment_hash, timestamp, sender_info):
+    def __init__(self, file_id, fragment_id, fragment_size, fragment_hash, timestamp, sender_info):
         self.file_id = file_id
         self.fragment_id = fragment_id
-        self.fragment_index = fragment_index
         self.fragment_size = fragment_size
         self.fragment_hash = fragment_hash
         self.timestamp = timestamp
@@ -14,7 +13,6 @@ class FileMetadata:
         return {
             'file_id': self.file_id,
             'fragment_id': self.fragment_id,
-            'fragment_index': self.fragment_index,
             'fragment_size': self.fragment_size,
             'fragment_hash': self.fragment_hash,
             'timestamp': self.timestamp,
