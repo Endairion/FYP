3b7241d85be9ad7f30d4fcf12ccb38921d810d80
Endairion <86313950+Endairion@users.noreply.github.com>
2023-10-21 02:56:38 +0800

    added ip for both test machine

--- peer1.py
+++ peer1.py
@@ -2,7 +2,7 @@ from node import Node
 
 def main():
     node1 = Node()
-    node1.ip = '0.0.0.0'  # set the IP address to the public IP of the machine
+    node1.ip = '35.240.242.166'  # set the IP address to the public IP of the machine
     node1.listen_and_connect()
 
 
