3ae525f0bbb61711072d7f128cee7d0d2474750d
Endairion <86313950+Endairion@users.noreply.github.com>
2023-10-23 00:41:18 +0800

    adding threading to peer.py

--- peer1.py
+++ peer1.py
@@ -1,8 +1,20 @@
+import threading
 from node import Node
+
+def connect_peer(node):
+    node.connect_to_peer('34.143.221.135')  # Connect to Peer 2 on port 8002
 
 if __name__ == "__main__":
     node = Node()  # Create a Node instance for Peer 1
     node.start()  # Start listening for incoming connections
-    
-    input("Press Enter to connect")
-    node.connect_to_peer('34.143.221.135')  # Connect to Peer 2 on port 8002
+
+    # Start a thread to handle the connection setup
+    connection_thread = threading.Thread(target=connect_peer, args=(node,))
+    connection_thread.start()
+
+    input("Press Enter to interact with the program")
+
+    # You can add your user interaction code here
+
+    # Wait for the connection setup thread to complete (optional)
+    connection_thread.join()
--- peer2.py
+++ peer2.py
@@ -1,8 +1,20 @@
+import threading
 from node import Node
+
+def connect_peer(node):
+    node.connect_to_peer('35.240.242.166')  # Connect to the other peer
 
 if __name__ == "__main__":
     node2 = Node()
-    node2.start()
+    node2.start()  # Start listening for incoming connections
 
-    input("Press Enter to connect")
-    node2.connect_to_peer('35.240.242.166')
+    # Start a thread to handle the connection setup
+    connection_thread = threading.Thread(target=connect_peer, args=(node2,))
+    connection_thread.start()
+
+    input("Press Enter to interact with the program")
+
+    # You can add your user interaction code here
+
+    # Wait for the connection setup thread to complete (optional)
+    connection_thread.join()
