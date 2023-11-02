01e2de4ef3ffa3ae8889722e8cfbdd8f091dc568
Endairion <86313950+Endairion@users.noreply.github.com>
2023-11-02 12:19:55 +0800

    add main ui

--- main.py
+++ main.py
@@ -1,50 +1,100 @@
-# main.py
-# Import necessary modules
-from blockchain import Blockchain  # Assuming Blockchain class is defined in 'blockchain.py' file
-from block import Block  # Assuming Block class is defined in 'block.py' file
-from metadata import FileMetadata  # Import the FileMetadata class from 'metadata.py'
-from fragment import Fragment  # Import the Fragment class from 'fragment.py'
-import time
+from PyQt5 import uic, QtCore, QtGui, QtWidgets
+import sys
 
+class MainWindow(QtWidgets.QMainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        uic.loadUi('ui_main.ui', self)
+
+        self.dragging = False
+        self.offset = QtCore.QPoint()
+        self.setWindowFlags(QtCore.Qt.FramelessWindowHint)
+        self.setAttribute(QtCore.Qt.WA_TranslucentBackground)
+
+        self.minimizeButton.clicked.connect(self.showMinimized)
+        self.maximizeButton.clicked.connect(self.toggleMaximize)
+        self.closeButton.clicked.connect(self.close)
+
+        # Set the initial CSS for the QPushButton
+        self.default_css = """
+        QPushButton {
+            border: none;
+            background-color: rgba(0,0,0,0);
+        }
+        QPushButton:hover {
+            background-color: rgb(91,90,90);
+        }
+        QPushButton:pressed {    
+            background-color: rgba(0,0,0,0);
+        }
+        """
+        self.active_css = """
+        QPushButton {
+            border: none;
+            background-color: rgb(91,90,90);
+        }
+        """
+
+        # Create a dictionary to map buttons to pages
+        self.button_page_map = {
+            self.uploadButton: self.page_upload,
+            self.downloadButton: self.page_download,
+            self.deleteButton: self.page_delete
+        }
+
+        # Connect the buttons to the activate_page method
+        for button in self.button_page_map.keys():
+            button.setStyleSheet(self.default_css)
+            button.clicked.connect(self.activate_page)
+
+        self.uploadButton.setStyleSheet(self.active_css)
+        self.stackedWidget.setCurrentWidget(self.page_upload)
+
+    def reset_button_styles(self):
+        # Reset the style of all buttons
+        for button in self.button_page_map.keys():
+            button.setStyleSheet(self.default_css)
+
+    def activate_page(self):
+        # Get the button that sent the signal
+        button = self.sender()
+
+        # Reset the styles of all buttons and set the style of the active button
+        self.reset_button_styles()
+        button.setStyleSheet(self.active_css)
+
+        # Set the current widget of the stackedWidget to the page associated with the button
+        self.stackedWidget.setCurrentWidget(self.button_page_map[button])
+    def mousePressEvent(self, event):
+        # Check if mouse is pressed within the form area
+        if event.button() == QtCore.Qt.LeftButton and self.rect().contains(event.pos()):
+            self.dragging = True
+            self.offset = event.pos()
+
+    def mouseMoveEvent(self, event):
+        # Move the window if the mouse is being dragged
+        if self.dragging:
+            self.move(self.pos() + event.pos() - self.offset)
+
+    def mouseReleaseEvent(self, event):
+        # Stop dragging the window when the mouse is released
+        if event.button() == QtCore.Qt.LeftButton:
+            self.dragging = False
+
+    def toggleMaximize(self):
+        # Toggle between maximized and normal window state
+        if self.isMaximized():
+            self.showNormal()
+            self.maximizeButton.setIcon(QtGui.QIcon("./icons/square.svg"))
+            self.maximizeButton.setToolTip("Maximize")
+        else:
+            self.showMaximized()
+            self.maximizeButton.setIcon(QtGui.QIcon("./icons/minimize.svg"))
+            self.maximizeButton.setToolTip("Restore")
 if __name__ == "__main__":
-    # Load the existing blockchain from a file
-    blockchain = Blockchain.load_from_file('blockchain.pkl')
+    app = QtWidgets.QApplication(sys.argv)
 
-    if blockchain is None:
-        # If the file doesn't exist or is empty, create a new blockchain
-        blockchain = Blockchain()
+    window = MainWindow()
+    window.show()
 
-        # Create and add the genesis block
-        genesis_block = blockchain.createGenesisBlock()
-        blockchain.addBlock(genesis_block)
-
-    # Create metadata entries for fragmented files
-    file_metadata_entries = [
-        # File 1
-        FileMetadata("file_id_1", [
-            Fragment("fragment_id_1_1", 1024, "hash1_1", "sender1_1", "hash1_2"),
-            Fragment("fragment_id_1_2", 2048, "hash1_2", "sender1_2")
-        ]),
-
-        # File 2
-        FileMetadata("file_id_2", [
-            Fragment("fragment_id_2_1", 1024, "hash2_1", "sender2_1", "hash2_2"),
-            Fragment("fragment_id_2_2", 2048, "hash2_2", "sender2_2")
-        ]),
-
-        # File 3
-        FileMetadata("file_id_3", [
-            Fragment("fragment_id_3_1", 1024, "hash3_1", "sender3_1", "hash3_2"),
-            Fragment("fragment_id_3_2", 2048, "hash3_2", "sender3_2")
-        ])
-    ]
-
-    # Create a new block for each file's metadata and add them to the existing blockchain
-    for file_metadata in file_metadata_entries:
-        block = Block(time.time(), [file_metadata.to_dict()])
-        blockchain.addBlock(block)
-
-
-    # Save the updated blockchain back to the file
-    print(blockchain)
-    blockchain.save_to_file('blockchain.pkl')
+    sys.exit(app.exec_())
