d4b5b6756d430deae207a9b9572f1ec18406c815
Endairion <86313950+Endairion@users.noreply.github.com>
2023-11-29 19:24:37 +0800

    hotfix

--- blockchain.py
+++ blockchain.py
@@ -54,7 +54,10 @@ class Blockchain:
         file_info_list = []
 
         # Iterate over each block in the chain
-        for block in self.chain:
+        for serialized_block in self.chain:
+            # Deserialize the block
+            block = pickle.loads(serialized_block)
+
             # Extract the file information from the block's data
             file_info = block.data  # Modify this line if the file information is stored differently
 
