7b90ee3432d27fcf5001d24e166cd6c4da48fbe9
Endairion <pmliew@gmail.com>
2023-11-30 00:26:00 +0800

    hotfix

--- login.py
+++ login.py
@@ -377,12 +377,21 @@ class MainWindow(QtWidgets.QMainWindow):
                 formatted_file_info = {
                     "file_id": file_info.file_id,
                     "file_name": file_info.file_name,
-                    "file_size": file_info.file_size,
+                    "file_size": self.convert_size(file_info.file_size),
                     "sender": file_info.sender,
                 }
                 formatted_file_info_list.append(formatted_file_info)
 
         return formatted_file_info_list
+    
+    def convert_size(size_bytes):
+        if size_bytes == 0:
+            return "0B"
+        size_name = ("B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB")
+        i = int(math.floor(math.log(size_bytes, 1024)))
+        p = math.pow(1024, i)
+        s = round(size_bytes / p, 2)
+        return "%s %s" % (s, size_name[i])
 
 
 
