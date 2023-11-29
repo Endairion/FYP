a7b9a6e04bb7018e859fb5c2fce004c064ce5731
Endairion <pmliew@gmail.com>
2023-11-29 23:24:02 +0800

    hotfix

--- test.py
+++ test.py
@@ -4,7 +4,13 @@ def read_blockchain():
     # Load the blockchain from the file
     blockchain = Blockchain.load_from_file('blockchain.pkl')
 
+    blocks = blockchain.extract_file_info()
+    
     if blockchain is not None:
+        # Enumerate the blocks and print their types
+        for i, block in enumerate(blocks):
+            print(f"Block {i}: {type(block)}")
+
         # Print each block of the blockchain
         blockchain.print_blocks()
     else:
